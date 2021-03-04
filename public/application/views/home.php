eeed4901280423aebf48cf028e8f6e4ebb914ba6
Lauranja1234 <lauravitoria5657@gmail.com>
2021-03-04 15:24:01 +0000

    modificando tela inicial e configurando bootstrap

--- public/application/views/home.php
+++ public/application/views/home.php
@@ -6,11 +6,37 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="UTF-8">
 	<title>Site PW3</title>
+	<link rel="stylesheet" href="<?php echo base_url()?>assets/reset.css">
+	<link rel="stylesheet" href="<?php echo base_url()?>assets/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo base_url()?>assets/css/principal.css">
 </head>
 <body>
-
-<h1>Seja bem-vindo!</h1>
-
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+			<div class="jumbotron cont">
+				<h1 class="display-4">Seja bem-vindo!</h1>
+				<p class="lead">Último ano de ETEC! (aprendendo PHP usando framework)</p>
+				<hr class="my-4">
+			</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12">
+				<ul class="nav">
+					<li class="nav-item">
+						<a class="nav-link active" href="#">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Config</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Logout</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
 
