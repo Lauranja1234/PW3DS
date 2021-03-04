0cf5914167b6874b222f70606e02d28de225b0f9
Lauranja1234 <lauravitoria5657@gmail.com>
2021-03-04 14:29:42 +0000

    configurando framework e pagina inicial/controller

---
+++ public/application/views/home.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<title>Site PW3</title>
+</head>
+<body>
+
+<h1>Seja bem-vindo!</h1>
+
+</body>
+</html>
+
