913c0451b5d1395cf9e50483236d02033dc6bb60
Lauranja1234 <lauravitoria5657@gmail.com>
2021-03-25 14:43:43 +0000

    por segurança

---
+++ public/application/controllers/Pagina2.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Pagina2 extends CI_Controller {
+
+	public function index()
+	{
+		$this->template->show('pagina2');
+	}
+}
---
+++ public/application/views/pagina2.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<main>
+	<div class="row">
+		<div class="col-12">
+			<h1>Livros</h1>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-12">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima, cum pariatur soluta quasi consequuntur corporis adipisci molestiae enim ducimus facilis nisi saepe cupiditate reiciendis eos, officiis minus, commodi animi ab!
+		Blanditiis earum eaque reprehenderit necessitatibus perspiciatis, autem aperiam sed praesentium cupiditate ipsa voluptates voluptatem recusandae quaerat porro quae labore aliquam nam inventore debitis deserunt? Laborum error sapiente libero? Quis, autem.
+		Dolorem iure, fuga harum perferendis placeat nobis aliquam, minus illum similique consectetur, accusantium fugit assumenda architecto atque aspernatur delectus suscipit odio doloribus incidunt pariatur obcaecati veritatis deleniti! Optio, suscipit dolorem.</div>
+	</div>
+</main>
